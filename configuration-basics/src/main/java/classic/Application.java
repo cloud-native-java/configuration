a030fa22e351ae9ac18fbffe50a434a973a7b81a
Josh Long <josh@joshlong.com>
2015-11-24 21:00:58 -0800

    fixing builds

--- configuration-basics/src/main/java/classic/Application.java
+++ configuration-basics/src/main/java/classic/Application.java
@@ -4,11 +4,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
-    public void setConfigurationProjectName(String pn) {
-        System.out.println("the configuration project name is " + pn);
-    }
-
     public static void main(String[] args) {
         new ClassPathXmlApplicationContext("classic.xml");
+    }
+
+    public void setConfigurationProjectName(String pn) {
+        System.out.println("the configuration project name is " + pn);
     }
 }
