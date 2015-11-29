e676d8e72ef5274099893737be41e552ac34e33b
Josh Long <josh@joshlong.com>
2015-11-29 00:28:14 -0800

    fixing build

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,10 @@ $ROOT_DIR/cf-common.sh
 # find its URL
 # then set it as a variable when deploying the config client
 # as a result, there is a NOOP in the config-server directory
+
+
+
+deploy_app configuration-service
+deploy_service configuration-service
+
+deploy_app configuration-client
