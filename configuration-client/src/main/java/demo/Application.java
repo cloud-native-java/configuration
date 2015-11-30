71b57610f5c9bb7b65339247d2d5a5146790e676
Josh Long <josh@joshlong.com>
2015-11-30 20:46:14 +0800

    updated

--- configuration-client/src/main/java/demo/Application.java
+++ configuration-client/src/main/java/demo/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main( tring[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
