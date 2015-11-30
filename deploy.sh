ef89b5af9e649537e6fdc8f440b615878d2cb710
Josh Long <josh@joshlong.com>
2015-11-30 19:30:45 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ source ./build/utils/cf-common.sh
 
 cs=configuration-service
 
+cf d -f configuration-client
+cf d -f $cs
 cf s | grep $cs && cf ds -f $cs
+
 deploy_app $cs
 deploy_service $cs
 deploy_app configuration-client
