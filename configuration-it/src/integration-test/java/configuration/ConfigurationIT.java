98e07a74f18fd8d79b5f479786442d608332404b
Josh Long <josh@joshlong.com>
2017-04-13 10:31:30 -0700

    initial commits of ITs

--- configuration-it/src/integration-test/java/configuration/ConfigurationIT.java
+++ configuration-it/src/integration-test/java/configuration/ConfigurationIT.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
